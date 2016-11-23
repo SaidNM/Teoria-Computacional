f62e97e2f79977da719f0442703da608d421b767
SaidNM <SaidNM@users.noreply.github.com>
2016-11-23 12:36:43 -0600

    Pila bien

--- AutomataPila/AutomataPila/src/automata/AutomataPila.java
+++ AutomataPila/AutomataPila/src/automata/AutomataPila.java
@@ -1,8 +1,20 @@
 
 package automata;
 
+import sources.Pila;
+
 
 public class AutomataPila {
-    private int dato;
+    public static void main(String[] args) {
+         Pila automata=new Pila();
+         
+         System.out.println(automata.is_empty());
+         automata.push(0);
+         automata.push(15);
+         automata.push(3);
+         automata.pop();
+         automata.pop();
+
+    }
     
 }
--- AutomataPila/AutomataPila/src/sources/Pila.java
+++ AutomataPila/AutomataPila/src/sources/Pila.java
@@ -19,20 +19,24 @@ public void push(int dato){
         nuevo.setSiguiente(tope);
         tope=nuevo;
     }
+    System.out.println(tope.getDato());
 }
 
-public void pop(){
+public int pop(){
     Nodo auxiliar;
+    int dato=0;
     if(!is_empty()){
         auxiliar=tope;
         tope=tope.getSiguiente();
+        dato=auxiliar.getDato();
         auxiliar=null;
         
     }
     else{
         System.out.println("Pila vacia");
     }
-    
+    System.out.println(tope.getDato());
+    return dato;
 }    
 
 public boolean is_empty(){
